317fd0cda2febe30e9e7b8ca726d90136f9db5b8
danielchirs <danielchirs@sina.com>
2015-02-12 09:25:41 +0800

    v1.0

--- app/src/main/java/com/reggie/au/autohome/AuActivityHouse.java
+++ app/src/main/java/com/reggie/au/autohome/AuActivityHouse.java
@@ -1,9 +1,8 @@
 package com.reggie.au.autohome;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.tandong.sa.activity.SmartActivity;
 import com.tandong.sa.tools.AssistTool;
@@ -11,14 +10,42 @@ import com.tandong.sa.tools.AssistTool;
 
 public class AuActivityHouse extends SmartActivity {
 
+    private Button btnl;
+    private Button btnr;
+    private AssistTool assistTool;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AssistTool.setFullScreen(this);
         setContentView(R.layout.activity_au_activity_house);
+        assistTool = new AssistTool(this);
+        btnl=(Button)this.findViewById(R.id.btn_l);
+        btnr=(Button)this.findViewById(R.id.btn_r);
+
+        btnl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                assistTool.showToast("用户信息");
+            }
+        });
+
+        btnr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                assistTool.showToast("功能按键");
+            }
+        });
 
 
     }
+
+
+
+
+
+
 
 
 
