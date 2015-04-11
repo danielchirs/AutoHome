3ae99ccd7b447b51cb0c6a513aab7e92dec12e08
danielchirs <danielchirs@sina.com>
2015-04-11 21:50:32 +0800

    zy sql1.3

--- app/src/main/java/com/reggie/au/autohome/utils/DomXml.java
+++ app/src/main/java/com/reggie/au/autohome/utils/DomXml.java
@@ -2,6 +2,7 @@ package com.reggie.au.autohome.utils;
 
 import com.reggie.au.autohome.command.SmtechData;
 import com.reggie.au.autohome.view.SmtechDeviceView;
+import com.reggie.au.autohome.view.SmtechHouseInfoView;
 import com.reggie.au.autohome.view.SmtechHouseView;
 import com.reggie.au.autohome.view.SmtechSceneModeView;
 
@@ -23,47 +24,88 @@ public class DomXml {
 
     //读取XML配置文件全路径
     public static void Personxml(String path,int type)throws Exception {
+        if(type==0){//房子基本信息
+            analyInfo(path);
+        }else if (type==1){//房子控制配置
+            analyData(path);
+        }else if (type==2){//房子情景配置
+            analyMod(path);
+        }
+    }
 
-        if(type==0){//读取情景模式
-            String readString=getxml(path);
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            factory.setNamespaceAware(true);
-            XmlPullParser xmlParser = factory.newPullParser();
-            xmlParser.setInput(new StringReader(readString));
-            int evtType= xmlParser.getEventType();
-            SmtechSceneModeView smtechSceneModeView=null;
-            while(evtType!=XmlPullParser.END_DOCUMENT){
-                switch(evtType){
-                    case XmlPullParser.START_TAG:
-                        String name=xmlParser.getName();
-                        if("mode".equals(name)){
-                            smtechSceneModeView=new SmtechSceneModeView();
-                            smtechSceneModeView.id=Integer.parseInt(xmlParser.getAttributeValue(0));
-                        }
-                        if("name".equals(name)){
-                            smtechSceneModeView.name=xmlParser.nextText();
-                        }
-                        if("icon".equals(name)){
-                            smtechSceneModeView.icon=xmlParser.nextText();
-                        }
-                        if("command".equals(name)){
-                            smtechSceneModeView.command=xmlParser.nextText();
-                        }
-                        break;
-                    case XmlPullParser.END_TAG://结束元素事件
-                        if (xmlParser.getName().equalsIgnoreCase("mode"))
-                        {
-                            SmtechData.sceneMap.put(smtechSceneModeView.id+"",smtechSceneModeView);
-                            smtechSceneModeView=null;
-                        }
-                        break;
-                    default:
-                        break;
-                }
-                //如果xml没有结束，则导航到下一个节点
-                evtType=xmlParser.next();
+    /**
+     * 解析房子基础信息
+     * @param path
+     * @throws Exception
+     */
+    public static void analyInfo(String path) throws Exception {
+        String readString=getxml(path);
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        XmlPullParser xmlParser = factory.newPullParser();
+        xmlParser.setInput(new StringReader(readString));
+        int evtType= xmlParser.getEventType();
+        SmtechHouseInfoView houseInfoView = null;
+        while(evtType!=XmlPullParser.END_DOCUMENT){
+            switch(evtType){
+                case XmlPullParser.START_TAG:
+                    String name=xmlParser.getName();
+                    if("house".equals(name)){
+                        houseInfoView=new SmtechHouseInfoView();
+                    }
+                    if("housename".equals(name)){
+                        houseInfoView.setHouseName(xmlParser.nextText());
+                    }
+                    if("user".equals(name)){
+                        houseInfoView.setHouseUser(xmlParser.nextText());
+                    }
+                    if("address".equals(name)){
+                        houseInfoView.setHouseAddress(xmlParser.nextText());
+                    }
+                    if("tel".equals(name)){
+                        houseInfoView.setHouseTel(xmlParser.nextText());
+                    }
+                    if("mobile".equals(name)){
+                        houseInfoView.setHouseMobile(xmlParser.nextText());
+                    }
+
+                    if("ip".equals(name)){
+                        houseInfoView.setIp(xmlParser.nextText());
+                    }
+                    if("domain".equals(name)){
+                        houseInfoView.setDomain(xmlParser.nextText());
+                    }
+                    if("prot".equals(name)){
+                        houseInfoView.setPort(Integer.parseInt(xmlParser.nextText()));
+                    }
+                    if("name".equals(name)){
+                        houseInfoView.setLoginName(xmlParser.nextText());
+                    }
+                    if("password".equals(name)){
+                        houseInfoView.setPassword(xmlParser.nextText());
+                    }
+                    break;
+                case XmlPullParser.END_TAG://结束元素事件
+                    if (xmlParser.getName().equalsIgnoreCase("house"))
+                    {
+                        SmtechData.houseInfo=houseInfoView;
+                    }
+                    break;
+                default:
+                    break;
             }
-        }else if(type==1){//读取数据配置
+            //如果xml没有结束，则导航到下一个节点
+            evtType=xmlParser.next();
+        }
+
+    }
+
+    /**
+     * 解析房子指令配置数据
+     * @param path
+     * @throws Exception
+     */
+    public static void analyData(String path) throws Exception {
             String readString=getxml(path);
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             factory.setNamespaceAware(true);
@@ -118,8 +160,63 @@ public class DomXml {
                 //如果xml没有结束，则导航到下一个节点
                 evtType=xmlParser.next();
             }
-        }
+    }
 
+    /**
+     * 解析房子情景模式
+     * @param path
+     * @throws Exception
+     */
+    public static void analyMod(String path) throws Exception {
+        String readString=getxml(path);
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        XmlPullParser xmlParser = factory.newPullParser();
+        xmlParser.setInput(new StringReader(readString));
+        int evtType= xmlParser.getEventType();
+        SmtechSceneModeView smtechSceneModeView=null;
+        while(evtType!=XmlPullParser.END_DOCUMENT){
+            switch(evtType){
+                case XmlPullParser.START_TAG:
+                    String name=xmlParser.getName();
+                    if("mode".equals(name)){
+                        smtechSceneModeView=new SmtechSceneModeView();
+                    }
+                    if("id".equals(name)){
+                        smtechSceneModeView.setId(Integer.parseInt(xmlParser.nextText()));
+                    }
+                    if("name".equals(name)){
+                        smtechSceneModeView.setName(xmlParser.nextText());
+                    }
+                    if("icon".equals(name)){
+                        smtechSceneModeView.setIcon(xmlParser.nextText());
+                    }
+                    if("machinecode".equals(name)){
+                        smtechSceneModeView.setMachinecode(xmlParser.nextText());
+                    }
+                    if("function".equals(name)){
+                        smtechSceneModeView.setFunction(xmlParser.nextText());
+                    }
+                    if("address".equals(name)){
+                        smtechSceneModeView.setAddress(xmlParser.nextText());
+                    }
+                    if("item".equals(name)){
+                        smtechSceneModeView.getItem().put(xmlParser.getAttributeValue(0),xmlParser.nextText());
+                    }
+                    break;
+                case XmlPullParser.END_TAG://结束元素事件
+                    if (xmlParser.getName().equalsIgnoreCase("mode"))
+                    {
+                        SmtechData.sceneMap.put(smtechSceneModeView.getId()+"",smtechSceneModeView);
+                        smtechSceneModeView=null;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            //如果xml没有结束，则导航到下一个节点
+            evtType=xmlParser.next();
+        }
     }
 
 
--- app/src/main/java/com/reggie/au/autohome/view/SmtechSceneModeView.java
+++ app/src/main/java/com/reggie/au/autohome/view/SmtechSceneModeView.java
@@ -2,18 +2,99 @@ package com.reggie.au.autohome.view;
 
 import android.provider.ContactsContract;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by daniel on 2015/3/29.
  * 情景模式配置
  */
 public class SmtechSceneModeView {
-    public int id;
-    public String name;
-    public String icon;
-    public String command;
-
+    private int id;
+    private String name;
+    private String icon;
+    private String command;
+    private String machinecode;
+    private String function;
+    private String address;
+    private Map<String,String> item;
 
     public SmtechSceneModeView() {
-        super();
+        item=new HashMap<String,String>();
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getMachinecode() {
+        return machinecode;
+    }
+
+    public void setMachinecode(String machinecode) {
+        this.machinecode = machinecode;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Map<String, String> getItem() {
+        return item;
+    }
+
+    public void setItem(Map<String, String> item) {
+        this.item = item;
+    }
+
+    /**
+     * 获取字符串
+     * @param itemCode
+     * @return
+     */
+    public String getModeCode(String itemCode){
+        String strCode=this.getMachinecode()+this.getFunction()+this.getAddress()+this.getItem().get(itemCode)+"01";
+        return strCode;
     }
 }
