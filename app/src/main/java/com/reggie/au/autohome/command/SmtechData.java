e98b79f3f12aaf514d8fad2d37692dda730c545a
danielchirs <danielchirs@sina.com>
2015-06-28 23:59:05 +0800

    zy 1.4

--- app/src/main/java/com/reggie/au/autohome/command/SmtechData.java
+++ app/src/main/java/com/reggie/au/autohome/command/SmtechData.java
@@ -18,8 +18,10 @@ public class SmtechData {
     public static SmtechHouseInfoView houseInfo=new SmtechHouseInfoView();//房子基础信息
     public static List<SmtechHouseView> houseList=new ArrayList<SmtechHouseView>();//房间列表
     public static Map<String,List<SmtechDeviceView>> dataMap=new HashMap<String,List<SmtechDeviceView>>();//家居配置数据列表
-    public static Map<String,SmtechSceneModeView> modMap=new HashMap<String,SmtechSceneModeView>();//情景数据列表
+    public static List<SmtechSceneModeView> modList=new ArrayList<SmtechSceneModeView>();//情景数据列表
+//    public static Map<String,SmtechSceneModeView> modMap=new HashMap<String,SmtechSceneModeView>();//情景数据列表
     public static Socket sever;
+    public static Map<String,String> stratus=new HashMap<String,String>();//记录控件最后状态
     public SmtechData() {
         super();
     }
